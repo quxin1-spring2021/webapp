29f5b2adad949ab366ebe3188f8a6be3b93b211a
Enoch Qu <enochqu@outlook.com>
2021-02-10 17:07:18 -0800

    refactor demo to user

--- models/user.model.js
+++ models/user.model.js
@@ -27,4 +27,13 @@ module.exports = (sequelize, Sequelize) => {
     })
 
     return User;
+    authenticate;
   };
+
+  async function authenticate({ username, password }) {
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+    }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 module.exports = app => {
-    const tutorials = require("../controllers/tutorial.controller.js");
+    const tutorials = require("../controllers/user.controller.js");
   
     var router = require("express").Router();
   
