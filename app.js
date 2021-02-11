0137d325e78e844fa07e17bc1e6c35aa805b87af
Enoch Qu <enochqu@outlook.com>
2021-02-10 16:59:10 -0800

    basic mysql connection finished

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./models");
+
+const User = db.users;
+
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to enoch application." });
+});
+
+app.get('/1', (req, res) => {
+    const id = req.params.id;
+    const user = 
+    User.findOne(id)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving Tutorial with id=" + id
+        });
+      });
+  })
+
+
+
+
+app.put('/1', (req, res) => {
+    const id = req.params.id;
+    User.update(req.body, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Tutorial was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating Tutorial with id=" + id
+        });
+      });
+  })
+
+app.post('/1', (req, res) => {
+    // Validate request
+    if (!req.body.username) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+      return;
+    }
+  
+    // Create a Tutorial
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname
+    };
+  
+    // Save Tutorial in the database
+    User.create(user)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the User."
+        });
+      });
+  }
+)
+
+
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, Sequelize) => {
+
+
+    const User = sequelize.define("user", {
+      id: {
+        primaryKey: true,
+        type: Sequelize.UUID,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      firstname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    })
+
+    return User;
+  };
