15f2a84ab56efa70cc56962b7cf53496ade7629b
Enoch Qu <enochqu@outlook.com>
2021-03-18 10:24:59 -0700

    update sh env

--- codedeploy/dependencies.sh
+++ codedeploy/dependencies.sh
@@ -6,9 +6,9 @@ cd /home/ubuntu/webapp
 pwd
 ls -al
 sudo npm install
-/bin/echo 'RDS_HOSTNAME = $RDS_HOSTNAME' > /home/ubuntu/webapp/.env
-/bin/echo 'RDS_USERNAME = $RDS_USERNAME' >> /home/ubuntu/webapp/.env
-/bin/echo 'RDS_PASSWORD = $RDS_PASSWORD' >> /home/ubuntu/webapp/.env
-/bin/echo 'RDS_DATABASE = $RDS_DATABASE' >> /home/ubuntu/webapp/.env
-/bin/echo 'RDS_PORT = $RDS_PORT' >> /home/ubuntu/webapp/.env
-/bin/echo 'BUCKET_NAME = $BUCKET_NAME' >> /home/ubuntu/webapp/.env
+echo 'RDS_HOSTNAME = ${RDS_HOSTNAME}' > /home/ubuntu/webapp/.env
+echo 'RDS_USERNAME = ${RDS_USERNAME}' >> /home/ubuntu/webapp/.env
+echo 'RDS_PASSWORD = ${RDS_PASSWORD}' >> /home/ubuntu/webapp/.env
+echo 'RDS_DATABASE = $RDS_DATABASE' >> /home/ubuntu/webapp/.env
+echo 'RDS_PORT = $RDS_PORT' >> /home/ubuntu/webapp/.env
+echo 'BUCKET_NAME = $BUCKET_NAME' >> /home/ubuntu/webapp/.env
