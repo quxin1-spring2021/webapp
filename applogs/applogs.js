b48dddc8462fc8ad49896edc631338cb048f54c4
Enoch Qu <enochqu@outlook.com>
2021-03-24 17:08:19 -0700

    revert applogs

--- applogs/applogs.js
+++ applogs/applogs.js
@@ -2,12 +2,19 @@ const winston = require('winston');
 
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.simple(),
-    transports: [
-        new winston.transports.File({
-            filename: 'csye6225.log'
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
         }),
-    ],
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.json()
+    ),
+    defaultMeta: { service: 'csye6225-webapp' },
+    transports: [
+        new winston.transports.File({ filename: '/home/ubuntu/webapp/applogs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: '/home/ubuntu/webapp/applogs/webapp-combined.log' })
+    ]
 });
 
 module.exports = logger;
