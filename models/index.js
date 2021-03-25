7f473bbc89fbac8600dab7a798fc02b1292d288f
Enoch Qu <enochqu@outlook.com>
2021-03-25 01:43:13 -0700

    benchmark

--- models/index.js
+++ models/index.js
@@ -4,6 +4,8 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
+  benchmark: true,
+  logging: false,
   operatorsAliases: false,
 
   pool: {
